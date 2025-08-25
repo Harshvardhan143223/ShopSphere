dd505890b22cf8a58f006dd6b76592b8f205f998
Harshvardhan143223 <174183869+Harshvardhan143223@users.noreply.github.com>
2025-08-25 21:49:21 +0530

    update

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ server.use("/auth", authRouter.router);
 server.use("/cart", isAuth(), cartRouter.router);
 server.use("/orders", isAuth(), ordersRouter.router);
 
-server.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+//server.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 // Passport Strategies
 passport.use(
   "local",

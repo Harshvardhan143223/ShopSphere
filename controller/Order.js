dc9979892ce36850c7c9d399322c77c24514d253
Harshvardhan143223 <174183869+Harshvardhan143223@users.noreply.github.com>
2025-08-26 15:19:46 +0530

    update

--- controller/Order.js
+++ controller/Order.js
@@ -43,30 +43,32 @@ exports.updateOrder = async (req, res) => {
   }
 };
 
-exports.fetchAllOrders = async (req, res) => {
-  // sort = {_sort:"price",_order="desc"}
-  // pagination = {_page:1,_limit=10}
-  let query = Order.find({ deleted: { $ne: true } });
-  let totalOrdersQuery = Order.find({ deleted: { $ne: true } });
-
-  if (req.query._sort && req.query._order) {
-    query = query.sort({ [req.query._sort]: req.query._order });
-  }
-
-  const totalDocs = await totalOrdersQuery.countDocuments();
-  console.log({ totalDocs });
-
-  if (req.query._page && req.query._limit) {
-    const pageSize = req.query._limit || 10;
-    const page = req.query._page || 1;
-    query = query.skip(pageSize * (page - 1)).limit(pageSize);
-  }
-
-  try {
-    const docs = await query.exec();
-    res.set("X-Total-Count", totalDocs);
-    res.status(200).json({ orders: docs, totalOrders: totalDocs });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
+ exports.fetchAllOrders = async (req, res) => {
+    // sort = {_sort:"price",_order="desc"}
+    // pagination = {_page:1,_limit=10}
+    let query = Order.find({deleted:{$ne:true}});
+    let totalOrdersQuery = Order.find({deleted:{$ne:true}});
+  
+    
+    if (req.query._sort && req.query._order) {
+      query = query.sort({ [req.query._sort]: req.query._order });
+    }
+  
+    const totalDocs = await totalOrdersQuery.count().exec();
+    console.log({ totalDocs });
+  
+    if (req.query._page && req.query._limit) {
+      const pageSize = req.query._limit;
+      const page = req.query._page;
+      query = query.skip(pageSize * (page - 1)).limit(pageSize);
+    }
+  
+    try {
+      const docs = await query.exec();
+      res.set('X-Total-Count', totalDocs);
+      res.status(200).json(docs);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  };
+  
